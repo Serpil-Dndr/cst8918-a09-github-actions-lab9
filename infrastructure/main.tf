daa6894637330f10f14e7f86246f01f419f0aae2
Serpil-Dndr <serpil.dndrca@gmail.com>
2025-03-07 23:19:36 -0500

    test error

---
+++ infrastructure/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-dund0021"
+  location = "East US"
+
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "storagedund0021" # Change to a unique name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
